b09fa2723e04aab028e87a04dccad969925d3217
Alexander Brown <alex.brown@brownhaus.xyz>
2021-12-13 15:33:08 -0700

    I think its good

--- contMeas.m
+++ contMeas.m
@@ -10,7 +10,7 @@
 %
 % function [ a_meas ] = contMeas(x, input)
 function [ a_meas ] = contMeas(x_new, x_old, input)
-    simpar = input.simpar
+    simpar = input.simpar;
     s_old  = extract_state(x_old, simpar, 'nav');
     s_new  = extract_state(x_new, simpar, 'nav');
 
